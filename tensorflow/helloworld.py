6ca52718570b644a6759eb1f085d8de537a0bab1
johnnycc_huang <johnnycc_huang@compal.com>
2017-07-31 09:59:16 +0800

    softmax & word2vec

--- tensorflow/helloworld.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-import tensorflow as tf
-
-node1 = tf.constant(3.0, dtype=tf.float32)
-node2 = tf.constant(4.0)
-
-sess = tf.Session()
-print(sess.run([node1,node2]))
